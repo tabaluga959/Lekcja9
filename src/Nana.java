1c88c16dc344530124380ca5b5bda01b03d39648
Damian <tabaluga959@gmail.com>
2018-06-10 13:58:32 +0200

    już działa

--- src/Nana.java
+++ src/Nana.java
@@ -1,6 +1,12 @@
 public class Nana {
+
     public static void main(String[] args) {
         System.out.println("Czemu to kurła nie działa?!");
         System.out.println("blać");
+
+
+        System.out.println("Czemu to kurła działa");
+        System.out.println("blać");
     }
+
 }
